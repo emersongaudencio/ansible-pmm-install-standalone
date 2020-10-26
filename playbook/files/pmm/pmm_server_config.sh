0d3fa1a281f68043d609888a41c97f5d06ef5514
Emerson Gaudencio <emerson.fender@gmail.com>
2020-10-26 17:06:32 +0000

    Starting and Enabling - Docker service

--- playbook/files/pmm/pmm_server_config.sh
+++ playbook/files/pmm/pmm_server_config.sh
@@ -3,6 +3,10 @@ echo "HOSTNAME: " `hostname`
 echo "BEGIN - [`date +%d/%m/%Y" "%H:%M:%S`]"
 echo "##############"
 
+# Starting and Enabling - Docker service
+systemctl enable docker.service
+systemctl start docker.service
+
 # Pull the latest 2.x image
 docker pull percona/pmm-server:2
 
